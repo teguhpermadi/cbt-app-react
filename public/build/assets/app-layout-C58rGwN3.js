3478ed69df514ab3b6855b6261212b47363ff172
teguhpermadi <tguh.permadi@gmail.com>
2026-01-21 14:53:18 +0700

    feat : vps deploy

--- public/build/assets/app-layout-C58rGwN3.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-DyN-wpxS.js";import{A as s}from"./app-sidebar-layout-C8FOlvLm.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
--- public/build/assets/square-D7hxKJse.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./button-CdyPjVIa.js";const t=[["rect",{width:"18",height:"18",x:"3",y:"3",rx:"2",key:"afitv7"}]],o=e("Square",t);export{o as S};
